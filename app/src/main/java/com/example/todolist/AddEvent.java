2bc954b8b8dcef299e6e2f093ac9a8119bf43dc3
Gustavo Thiodorus <gustavothiodorus10@gmail.com>
2019-11-30 00:11:45 +0700

    udah tak tambain fungsi tapi gak mau jalan mungkin permissionnya :(

--- app/src/main/java/com/example/todolist/AddEvent.java
+++ app/src/main/java/com/example/todolist/AddEvent.java
@@ -1,110 +1,100 @@
 package com.example.todolist;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import java.util.Calendar;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link AddEvent.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link AddEvent#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class AddEvent extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class AddEvent extends Fragment implements View.OnClickListener {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
+    private EditText inputJudul, inputDeskripsi;
+    private Button btnDeadlineData, btnAddImage;
+    private TextView txtDeadlineDate;
+    private Calendar c;
+    private DatePickerDialog dpd;
+    private int mYear,mMonth,mDay;
+    private ImageView img;
+    public static final int PICK_IMAGE = 1000;
 
     public AddEvent() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddEvent.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddEvent newInstance(String param1, String param2) {
-        AddEvent fragment = new AddEvent();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        inputJudul = (EditText) getActivity().findViewById(R.id.inputJudul);
+        inputDeskripsi = (EditText) getActivity().findViewById(R.id.inputDeskripsi);
+        btnDeadlineData = (Button) getActivity().findViewById(R.id.btnDeadlineDate);
+        btnAddImage = (Button) getActivity().findViewById(R.id.btnAddImage);
+        txtDeadlineDate = (TextView) getActivity().findViewById(R.id.txtDeadlineDate);
+
+        btnDeadlineData.setOnClickListener(this);
+        btnAddImage.setOnClickListener(this);
+
         return inflater.inflate(R.layout.fragment_add_event, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.btnDeadlineDate:
+                final Calendar c = Calendar.getInstance();
+                mYear = c.get(Calendar.YEAR);
+                mMonth = c.get(Calendar.MONTH);
+                mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(),
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                txtDeadlineDate.setText(dayOfMonth + "/" + (monthOfYear) + "/" + year);
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+                break;
+            case R.id.btnAddImage:
+                Intent intentImage = new Intent(Intent.ACTION_PICK);
+                intentImage.setType("image/*");
+                startActivityForResult(PICK_IMAGE, intentImage);
+                break;
         }
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+    private void startActivityForResult(int pickImage, Intent data) {
+        switch(pickImage) {
+            case PICK_IMAGE:
+                img.setImageURI(data.getData());
+                break;
         }
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
